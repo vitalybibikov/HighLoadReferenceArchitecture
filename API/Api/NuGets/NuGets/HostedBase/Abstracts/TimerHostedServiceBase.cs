ed4d0f01cbe0afa6ba7a8d849e9b8f167d8eea93
Vitali Bibikov <bibikovvitaly@gmail.com>
2021-01-14 00:09:41 +0300

    Added timer service

---
+++ API/Api/NuGets/NuGets/HostedBase/Abstracts/TimerHostedServiceBase.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace NuGets.NuGets.HostedBase.Abstracts
+{
+    public abstract class TimerHostedServiceBase : IHostedService, IDisposable
+    {
+        private readonly TimeSpan timeSpan;
+        private readonly ILogger logger;
+        private Timer timer = default!;
+        private CancellationTokenSource? cts;
+
+        protected TimerHostedServiceBase(TimeSpan timeSpan, ILogger logger)
+        {
+            this.timeSpan = timeSpan;
+            this.logger = logger;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            cts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
+
+            timer = new Timer(
+                OnTimer,
+                null,
+                timeSpan,
+                timeSpan);
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            timer.Change(Timeout.Infinite, 0);
+            cts?.Cancel();
+            cancellationToken.ThrowIfCancellationRequested();
+
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected abstract Task OnTimerAsync(object? state, CancellationToken cancellationToken);
+
+        protected virtual void Dispose(bool disposing)
+        {
+            timer.Dispose();
+            cts?.Dispose();
+        }
+
+        private async void OnTimer(object? state)
+        {
+            await OnTimerAsync(state, cts?.Token ?? CancellationToken.None);
+        }
+    }
+}
